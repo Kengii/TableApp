198ca08836fb94d2535b2d5666e00b89f0aef1a2
Kengii <97613101+Kengii@users.noreply.github.com>
2022-02-09 16:15:44 +0300

    Changed

---
+++ TableApp/TableApp/Controllers/SecondTVC.swift
@@ -0,0 +1,56 @@
+//
+//  SecondTVC.swift
+//  TableApp
+//
+//  Created by Владимир Данилович on 9.02.22.
+//
+
+import UIKit
+
+class SecondTVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+
+        return DataPerson.firstName.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let firstName = DataPerson.firstName[section]
+        let secondName = DataPerson.secondName[section]
+        let section = firstName + " " + secondName
+        return section
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        let email = DataPerson.email[indexPath.row]
+        let phone = DataPerson.numberPhone[indexPath.row]
+        cell.textLabel?.text = "\(email) \(phone)"
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
